9a2f279b98744fe3b47fddef65504cfda7af5f8b
Glen Egbert <glenegbert1@gmail.com>
2014-07-30 13:46:08 -0600

    Add functionality through Checking for Damage

---
+++ level-i/phone_book/lib/entry_repository.rb
@@ -0,0 +1,30 @@
+
+require_relative 'entry'
+require 'csv'
+
+class EntryRepository
+
+  attr_reader :entries
+
+  def self.load_entries(directory)
+    file = File.join(directory, 'people.csv')
+    data = CSV.open(file, headers: true, header_converters: :symbol)
+    rows = data.map do |row|
+      Entry.new(row)
+    end
+    new(rows)
+  end
+
+  def initialize(entries)
+    @entries = entries
+  end
+
+  def find_by_last_name(name)
+    entries.select { |entry| entry.last_name == name}
+  end
+
+  def find_by_first_and_last_name(first_name, last_name)
+    entries.select { |entry| entry.last_name == last_name && entry.first_name == first_name}
+  end
+
+end
